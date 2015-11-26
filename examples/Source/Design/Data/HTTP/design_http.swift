fad76ea7f266ae45bd070e2e4d56acfc612d1378
Pepi <pepibumur@gmail.com>
2015-11-26 19:33:12 +0700

    Extracted core component in a different Swift file

--- examples/Source/Design/Data/HTTP/design_http.swift
+++ examples/Source/Design/Data/HTTP/design_http.swift
@@ -2,52 +2,6 @@ import Foundation
 import ReactiveCocoa
 
 
-// MARK: - Request generator
-
-enum Method: String {
-    case POST, PUT, PATCH, DELETE, GET
-}
-
-enum HttpError: ErrorType {
-    case Default(NSError)
-}
-
-struct Session {
-    let accessToken: String
-    init(accessToken: String) {
-        self.accessToken = accessToken
-    }
-}
-
-private func request(baseURL: String)(path: String)(method: Method, parameters: [String: AnyObject])(session: Session) -> SignalProducer<(NSData, NSURLResponse), HttpError> {
-    return SignalProducer { (observer, disposable) in
-        
-        let urlSession: NSURLSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        let request: NSMutableURLRequest = NSMutableURLRequest(URL: NSURL(string: baseURL)!.URLByAppendingPathComponent(path))
-        // TODO - Append parameters
-        request.allHTTPHeaderFields = ["Authorization": "Bearer \(session.accessToken)"]
-        request.HTTPMethod = method.rawValue
-        let task = urlSession.dataTaskWithRequest(request, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
-            if let error = error {
-                observer.sendFailed(.Default(error))
-            }
-            else if let response = response, let data = data {
-                observer.sendNext((data, response))
-            }
-            observer.sendCompleted()
-        })
-        if disposable.disposed {
-            return
-        }
-        task.resume()
-    }
-}
-
-// MARK: - JSON requests
-
-func mapToJSON(input: (NSData, NSURLResponse)) -> AnyObject? {
-    return try! NSJSONSerialization.JSONObjectWithData(input.0, options: NSJSONReadingOptions.AllowFragments)
-}
 
 
 // MARK: - Mapping to plain object
@@ -83,14 +37,14 @@ struct User: Mappable {
     static func singleMapper(object: AnyObject) -> User? {
         guard let dict = object as? [String: AnyObject] else { return nil }
         // Extract the information from the dictionary
-        return Account(username: "xxxx", email: "xxx")
+        return User(username: "xxxx", email: "xxx")
     }
 }
 
 
 private func github() {
     // 1. Unauthenticated request for a given base url
-    let githubRequest = request("https://api.github.com")
+    let githubRequest = Reactive.HTTP.request("https://api.github.com")
     
     // 2. Unauthenticated request pointing to a given resource
     let userRequest = githubRequest(path: "/user")
@@ -99,7 +53,7 @@ private func github() {
     let showUserRequest = userRequest(method: .GET, parameters: [:])
     
     // 4. Authenticated request getting a resource
-    let mySession: Session = Session(accessToken: "xxx")
+    let mySession: Reactive.HTTP.Session = Reactive.HTTP.Session(accessToken: "token", refreshToken: "refresh")
     showUserRequest(session: mySession).on(event: { event in
         switch event {
         case .Next(_):
@@ -113,7 +67,7 @@ private func github() {
     
     // 5. Authenticated request getting a JSON resource
     showUserRequest(session: mySession)
-        .map(mapToJSON)
+        .map(Reactive.HTTP.mapToJSON)
         .on(event: { event in
             switch event {
             case .Next(_):
@@ -127,7 +81,7 @@ private func github() {
     
     // 6. Authenticated request getting an User resource
     showUserRequest(session: mySession)
-        .map(mapToJSON)
+        .map(Reactive.HTTP.mapToJSON)
         .ignoreNil()
         .map(User.singleMapper)
         .on(event: { event in
---
+++ examples/Source/Design/Data/HTTP/http.swift
@@ -0,0 +1,84 @@
+import Foundation
+import ReactiveCocoa
+
+public extension Reactive {
+    
+    public struct HTTP {
+        
+        // MARK: - REST Method
+        
+        public enum Method: String {
+            case POST, PUT, PATCH, DELETE, GET
+        }
+        
+        
+        // MARK: HTTP Error
+        
+        public enum HttpError: ErrorType {
+            case Default(NSError)
+        }
+        
+        
+        // MARK: - Session
+        
+        public struct Session {
+            
+            let accessToken: String
+            let refreshToken: String
+            
+            init(accessToken: String, refreshToken: String) {
+                self.accessToken = accessToken
+                self.refreshToken = refreshToken
+            }
+        }
+        
+        
+        // MARK: - FRP Interface
+        
+        static public func request(baseURL: String)(path: String)(method: Method, parameters: [String: AnyObject])(session: Session) -> SignalProducer<(NSData, NSURLResponse), HttpError> {
+            return SignalProducer { (observer, disposable) in
+                
+                let urlSession: NSURLSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+                var request: NSURLRequest = NSURLRequest(URL: NSURL(string: baseURL)!.URLByAppendingPathComponent(path))
+                request = self.urlRequest(request, withSession: session)
+                request = self.urlRequest(request, withMethod: method, parameters: parameters)
+                let task = urlSession.dataTaskWithRequest(request, completionHandler: { (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                    if let error = error {
+                        observer.sendFailed(.Default(error))
+                    }
+                    else if let response = response, let data = data {
+                        observer.sendNext((data, response))
+                    }
+                    observer.sendCompleted()
+                })
+                if disposable.disposed {
+                    return
+                }
+                task.resume()
+            }
+        }
+        
+        static public func mapToJSON(input: (NSData, NSURLResponse)) -> AnyObject? {
+            return try! NSJSONSerialization.JSONObjectWithData(input.0, options: NSJSONReadingOptions.AllowFragments)
+        }
+        
+        
+        // MARK: - Private Helpers
+        
+        static private func urlRequest(request: NSURLRequest, withSession session: Session) -> NSURLRequest {
+            let mutableRequest: NSMutableURLRequest = request.mutableCopy() as! NSMutableURLRequest
+            mutableRequest.allHTTPHeaderFields = ["Authorization": "Bearer \(session.accessToken)"]
+            return mutableRequest.copy() as! NSURLRequest
+        }
+        
+        static private func urlRequest(request: NSURLRequest, withMethod method: Method, parameters: [String: AnyObject]) -> NSURLRequest {
+            let mutableRequest: NSMutableURLRequest = request.mutableCopy() as! NSMutableURLRequest
+            mutableRequest.HTTPMethod = method.rawValue
+            
+            //TODO
+            return mutableRequest.copy() as! NSURLRequest
+        }
+        
+    }
+    
+}
