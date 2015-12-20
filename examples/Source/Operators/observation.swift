03772bca96fb3308e3119f1ee3fa236c97770cda
Pepi <pepibumur@gmail.com>
2015-12-20 14:06:20 +0100

    Reemplaced example by code with an image

--- examples/Source/Operators/observation.swift
+++ examples/Source/Operators/observation.swift
@@ -7,3 +7,9 @@ let (signal, observer) = Signal<String, NoError>.pipe()
 /// Does not forward any values from `self` until `trigger` sends a Next or
 /// Completed event, at which point the returned signal behaves exactly like
 /// `signal`.
+
+let textField: UITextField = UITextField()
+
+let searchStrings = textField.rac_textSignal()
+    .toSignalProducer()
+    .map { text in text as! String }
